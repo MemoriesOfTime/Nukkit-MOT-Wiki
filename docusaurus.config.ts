292c56e929a4ad17cad0c2c86558bcc4665adb20
Mcayear <mcayear@163.com>
2024-04-18 22:26:59 +0800

    patch: edit localized files.

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -48,6 +48,7 @@ const config: Config = {
           // Remove this to remove the "edit this page" links.
           editUrl:
             'https://github.com/MemoriesOfTime/Nukkit-MOT-Wiki/tree/master/',
+          editLocalizedFiles: true
         },
         blog: {
           showReadingTime: true,
@@ -55,6 +56,7 @@ const config: Config = {
           // Remove this to remove the "edit this page" links.
           editUrl:
             'https://github.com/MemoriesOfTime/Nukkit-MOT-Wiki/tree/master/',
+          editLocalizedFiles: true
         },
         theme: {
           customCss: './src/css/custom.css',
