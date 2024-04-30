13289828704b6efef5c0edc1b3282ccdae35bcb1
Mcayear <mcayear@163.com>
2024-04-30 10:50:09 +0800

    feat: 添加 plugin-ideal-image 实现图片骨架

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -174,6 +174,18 @@ const config: Config = {
       searchPagePath: 'search',
     },
   } satisfies Preset.ThemeConfig,
+  plugins: [
+    [
+      '@docusaurus/plugin-ideal-image',
+      {
+        quality: 70,
+        max: 1030, // max resized image's size.
+        min: 640, // min resized image's size. if original is lower, use that size.
+        steps: 2, // the max number of images generated between min and max (inclusive)
+        disableInDev: false,
+      },
+    ],
+  ]
 };
 
 export default config;
