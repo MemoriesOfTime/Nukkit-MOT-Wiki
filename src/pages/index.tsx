a377f784826f6a962736559ba55c30aa009b0967
Mcayear <mcayear@163.com>
2024-05-02 22:38:35 +0800

    Remove image resources and modify OG information (#12)
    * remove: 删除无用的图片
    
    * patch: 修改 og 的标题与图片

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -65,8 +65,8 @@ export default function Home(): JSX.Element {
   const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title="Nukkit-MOT Wiki"
+      description="Learn Nukkit-MOT and enjoy multi-version support and a rich plugin ecosystem.">
       <HomepageHeader />
       <main className={clsx(styles.heroMain)}>
         <HomepageFeatures />
