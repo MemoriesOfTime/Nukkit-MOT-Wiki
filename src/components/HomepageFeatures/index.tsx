7e4bb55694072bd64df41076424c2dc69e3c744d
BlusterySasha <blusterysasha@vk.com>
2024-08-20 15:51:22 +0400

    Update features (#19)

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -10,32 +10,29 @@ type FeatureItem = {
 
 const FeatureList: FeatureItem[] = [
   {
-    title: 'Easy to Use',
+    title: 'Version Support',
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Supports versions from 1.2 to the latest, allowing you to set the minimum protocol in the config for seamless gameplay.
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
+    title: 'AI Entity Support',
     Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Most entities with AI are fully supported, ensuring a dynamic and immersive environment for players.
       </>
     ),
   },
   {
-    title: 'Powered by React',
+    title: 'Vanilla Command Support',
     Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Fully supports vanilla commands, allowing you to manage and enhance gameplay with familiar commands.
       </>
     ),
   },
