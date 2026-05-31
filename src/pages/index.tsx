9d37ee46a928eed55a3986beebb0fafb3b5178fe
LT_Name <572413378@qq.com>
2026-05-31 21:57:08 +0800

    fix: resolve dependabot security alerts and fix JSX namespace errors

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -57,7 +57,7 @@ function HomepageHeader() {
   );
 }
 
-export default function Home(): JSX.Element {
+export default function Home(): React.ReactElement {
   useEffect(() => {
     redirectToLanguageVersion();
   }, []);
